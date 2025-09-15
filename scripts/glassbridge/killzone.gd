81d1a2f79c4a998fab9f0b9a35d63228fe07a09f
marufimtiaz <imaruf3113@gmail.com>
2025-09-16 01:41:07 +0600

    Add fall death

---
+++ scripts/glassbridge/killzone.gd
@@ -0,0 +1,13 @@
+extends Area3D
+
+@onready var timer: Timer = $Timer
+@onready var player: CharacterBody3D = $"../Player"
+
+func _on_body_entered(body: Node3D) -> void:
+	if body == player:
+		player.freeze()
+		timer.start()
+
+
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://scenes/startmenu.tscn")
