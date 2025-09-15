c3af2a331aaa010f4dab4f37271e08d5192a04a8
marufimtiaz <imaruf3113@gmail.com>
2025-09-16 05:56:17 +0600

    folder restructure

--- scripts/startmenu.gd
+++ scripts/startmenu.gd
@@ -7,7 +7,7 @@ func _on_start_game_1_pressed() -> void:
 
 
 func _on_start_game_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game2.tscn")
+	get_tree().change_scene_to_file("res://scenes/glassbridge/game2.tscn")
 
 
 func _on_quit_pressed() -> void:
