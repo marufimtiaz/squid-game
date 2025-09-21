5be93ab6523c500e66916204ce828823e4bca17e
marufimtiaz <imaruf3113@gmail.com>
2025-09-21 23:49:09 +0600

    add win game screen

--- scripts/glassbridge/glsbrg.gd
+++ scripts/glassbridge/glsbrg.gd
@@ -68,4 +68,5 @@ func _on_goal_body_entered(body: Node3D) -> void:
 
 func _on_timer_timeout() -> void:
 	player.freeze()
-	get_tree().change_scene_to_file("res://scenes/startmenu.tscn")
+	get_tree().change_scene_to_file("res://scenes/glassbridge/end_screen.tscn")
+	
